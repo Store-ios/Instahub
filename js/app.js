393241db9480df922a7be35bc9747e981ab7ea27
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2021-04-18 01:30:36 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -23,3 +23,30 @@ $("#tabs").fadeIn(2000);
 app.dialog.confirm("Follow us on Twitter to get our notification in time.","InstaHub",function(){
   window.location.href="https://twitter.com/instahubapp";
 });
+
+//block inspect element
+document.onkeydown = function (e) { 
+            if (event.keyCode == 123) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { 
+                return false; 
+            } 
+					} 
+//Disable right click
+document.onmousedown=disableclick;status="Right Click Disabled";function disableclick(event){  if(event.button==2)   {     alert(status);     return false;       }} 
+
+// init pull to refresh manually
+var ptr = app.ptr.create('.ptr-content');
+
+// or using get to retrieve already created instance
+var ptr = app.ptr.get('.ptr-content');
