937bb8bbf1dacbacf76f8a0c811e6be8eacff403
InstaTechHD <68559185+InstaTechHD@users.noreply.github.com>
2021-08-24 21:32:41 -0500

    Add files via upload

--- store/js/app.js
+++ store/js/app.js
@@ -94,7 +94,7 @@ function toggleWhiteTheme() {
 
 //Follow us on twitter
 app.dialog.confirm("Subscribe to our channel to get notifications in time.","InstaTechHD",function(){
-window.location.href="https://youtube.com/instahubapp";
+window.location.href="https://youtube.com/instatechhd";
 });
 
 //block inspect element
@@ -157,4 +157,49 @@ function toggleWhiteTheme() {
 		
 	}
 	
-	
+			//Request (Form)
+	window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
